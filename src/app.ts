ab0c836938c55fda85a11365b7f1cb46ae68dfd0
Jinxiu Chen <jinxiuchen@Jinxius-iMac.local>
2022-07-06 09:23:00 +1000

    feat: configure the routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import config from 'config';
 import connect from './utils/connnect';
 import log from './utils/logger';
+import routes from './routes'
 
 const port = config.get<number>('port') ? config.get<number>('port') : 3000;
 
@@ -10,4 +11,6 @@ const app = express();
 app.listen(port, async ()=>{
   log.info(`Server is working on the port ${port}.`);
   await connect();
+
+  routes(app);
 })
