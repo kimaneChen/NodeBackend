fea1f62fa378f2a72763768c15a715b2206b8627
Jinxiu Chen <jinxiuchen@Jinxius-iMac.local>
2022-07-06 15:33:27 +1000

    feat: build the user model

--- config/default.ts
+++ config/default.ts
@@ -1,15 +1,5 @@
 export default{
   port: 3000,
   dbUri: "mongodb+srv://apiUser:Oor12345678@cluster0.ps2npg9.mongodb.net/?retryWrites=true&w=majority",
+  saltWorkFactor: 10,
 }
-
-
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://apiUser:<password>@cluster0.ih7lgqr.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
---
+++ src/models/user.model.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+import config from 'config';
+import { string } from "zod";
+
+export interface UserDocument extends mongoose.Document {
+  email: string;
+  name: string;
+  password: string;
+  createdAt: Date;
+  updateAt: Date;
+}
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  name: { type: string, required: true },
+  password:{type: string, required: true } 
+},{
+  timestamps: true
+});
+
+userSchema.pre('save', async function(next: mongoose.HookNextFunction){
+  let user = this as unknown as UserDocument;
+
+  if(!user.isModified('password')){
+    return next()
+  }
+  
+  const salt= await bcrypt.genSalt(config.get('saltWorkFactor'));
+  
+  const hash = await bcrypt.hashSync(user.password, salt);
+
+  user.password = hash
+
+  return next();
+})
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
